6655263812d5fc6b05d171a4c9f1cbbb6751ac02
Fabian Pallares <fpallares@zeustecnologia.zeustecnologia.com>
2018-05-07 11:00:42 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    QRScanner,
+    InAppBrowser,
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  private urlguestservice:string;
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, private qrScanner: QRScanner) {
+  
+  }
+
+  gotozeuswebcheckin() {
+      //const browser = this.iab.create('http://169.46.167.242:8095/default.aspx?zic=ZEUSTEST', '_self', { location: 'no' });
+
+      const browser = this.iab.create(this.urlguestservice, '_self', { location: 'no' });
+
+  }
+
+  capturarQR() {
+    debugger;
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+
+
+          // start scanning
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+            this.urlguestservice=text;
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+          });
+
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
+}
